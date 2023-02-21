840a669561ff68544f60941cb9a1a93581c47b28
jason <ubuntu@ubuntu-20.04>
2023-02-22 04:05:39 +0900

    changed pin numbers for esp32 mini

--- .pio/libdeps/esp32dev/TFT_eSPI/User_Setup.h
+++ .pio/libdeps/esp32dev/TFT_eSPI/User_Setup.h
@@ -203,9 +203,9 @@
 // For ESP32 Dev board (only tested with ILI9341 display)
 // The hardware SPI can be mapped to any pins
 
-#define TFT_CS   15  // Chip select control pin
+#define TFT_CS   27  // Chip select control pin
 #define TFT_RST  -1  // Reset pin (could connect to RST pin)
-#define TFT_DC    2  // Data Command control pin
+#define TFT_DC    5  // Data Command control pin
 #define TFT_MOSI 23
 #define TFT_SCLK 18
 #define TFT_MISO 19
